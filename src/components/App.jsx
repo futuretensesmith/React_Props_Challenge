54075f7e523bf5933f9697d4b35be07016e86ff4
Kevin Smith <futuretense@gmail.com>
2021-02-16 18:26:14 -0800

    Another card and app edit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,21 @@ contacts.forEach(function (contact) {
 
 function App() {
   return (
-    <Card
-      name={contacts[0].name}
-      imgURL={contacts[0].imgURL}
-      phone={contacts[0].phone}
-      email={contacts[0].email}
-    />
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={contacts[0].name}
+        imgURL={contacts[0].imgURL}
+        phone={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        imgURL={contacts[1].imgURL}
+        phone={contacts[1].phone}
+        email={contacts[1].email}
+      />
+    </div>
   );
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,6 @@ import contacts from "../contacts";
 function Card(props) {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
